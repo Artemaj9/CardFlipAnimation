007332b807ed9e67c6305329d3040bbb7d3c9f54
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-24 01:11:55 +0300

    Initial Commit

---
+++ CardFlipAnimation/CardFlipAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlipAnimationApp.swift
+//  CardFlipAnimation
+//
+//  Created by Artem on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardFlipAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
